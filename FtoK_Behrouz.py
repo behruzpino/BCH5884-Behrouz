7ee7282d489d65137cc1a1b1886b950ab1222e3c
Behrouz <70213570+behruzpino@users.noreply.github.com>
2020-09-21 15:47:42 -0400

    Add files via upload

---
+++ FtoK_Behrouz.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+#https://github.com/behruzpino/BCH5884-Behrouz.git
+import math
+
+x=float(input("please enter the temparature in Fahrenheit and I will tell you that in Kelvin: "))
+
+print (x)
+y=(((x-32)*5)/9)+273.15
+
+print (y)
+
+print("right?!")
+
